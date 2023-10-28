750446f17fb0b0ffe3789bedd81c57c95608aeef
MasterJ3y <mjahangiry75@gmail.com>
2023-10-28 12:30:12 +0330

    Fix notif command issue

--- script.groovy
+++ script.groovy
@@ -35,7 +35,7 @@ def logOutOfRepository() {
 }
 
 def notif(message) {
-    sh 'RUST_LOG=debug ./notifier/telnotif -t $TEL_NOTIFIER_TOKEN' + "-r 6488784421 -m \"$message\""
+    sh 'RUST_LOG=debug ./notifier/telnotif -t $TEL_NOTIFIER_TOKEN' + " -r 6488784421 -m \"$message\""
 }
 
 return this
